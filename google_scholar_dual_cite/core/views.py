2c4e7e1779ebfb3aab73771e51f365fd2bc1f9fe
olga-kuzmina <olga-kuzmina@users.noreply.github.com>
2015-09-15 11:30:30 +0200

    Counting intersections

--- google_scholar_dual_cite/core/views.py
+++ google_scholar_dual_cite/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
 from django.http.response import JsonResponse, HttpResponseBadRequest
+import time
 from google_scholar_dual_cite.core.scholar_x import CitesScholarQuery
 from scholar import SearchScholarQuery, ScholarQuerier
 
@@ -25,9 +26,9 @@ def papers_by_query_api(request):
             result = {'papers': [{'title': '', 'id': 0, 'url': '', 'excerpt': ''}]}
         else:
             result = {'papers': [{'title': papers[0]['title'],
-                                    'id': papers[0]['cluster_id'],
-                                    'url': papers[0]['url'],
-                                    'excerpt': papers[0]['excerpt']}]}
+                                  'id': papers[0]['cluster_id'],
+                                  'url': papers[0]['url'],
+                                  'excerpt': papers[0]['excerpt']}]}
         return JsonResponse(result)
     else:
         return HttpResponseBadRequest()
@@ -39,11 +40,10 @@ def cites_api(request):
         page = request.GET.get('page', None)
         if not paper_id or page is None:
             return HttpResponseBadRequest()
-
         query = CitesScholarQuery(paper_id, page)
         querier = ScholarQuerier()
         querier.send_query(query)
-        papers = querier.papers
+        papers = querier.articles
 
         cites = []
         for paper in papers:
@@ -51,7 +51,7 @@ def cites_api(request):
                           'id': paper['cluster_id'],
                           'url': paper['url']})
 
-        return JsonResponse({'cites': cites})
+        return JsonResponse({'paper_id': paper_id, 'cites': cites})
     else:
         return HttpResponseBadRequest()
 
